ae6ed1773f33357e5c8fb7daa86a4b339569f2ee
OscarSaquil <osaquilb@miumg.edu.gt>
2023-06-17 07:38:13 -0600

    Correcciones al codigo en el examen

--- ExamenFinal_0902-22-12338/Program.cs
+++ ExamenFinal_0902-22-12338/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matriz = { 3, 2 };
+        //Intentamos asignar valores a los elementos de la matriz
+        matriz[0, 0] = 1;
+        matriz[0, 1] = 2;
+        matriz[1, 0] = 3;
+        matriz[1, 1] = 4;
+        matriz[2, 0] = 5;
+        matriz[2, 1] = 6;
+        //Intentamos imprimir los valores de la matriz
+        for(int i = 0; i < 3; i++) { 
+            for(int j = 0; j < 2; j++)
+            {
+                Console.WriteLine("Elemento en la posición [{0},{1}]:{2}", i, j, matriz[i,j]);
+            }
+        }
+        Console.ReadLine();
+    }
+}
